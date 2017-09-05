791b43cfc067c9c3bc014d7ced23b2e185081d51
BuildTools <mojweb123@gmail.com>
2017-09-05 20:18:42 +0200

    c

--- oct2016/c/main.cpp
+++ oct2016/c/main.cpp
@@ -22,32 +22,6 @@ void csort(int *p, int len)
     }
 }
 
-bool find3Numbers(int A[], int arr_size, int sum)
-{
-    int l, r;
-    csort(A, arr_size);
-    for (int i = 0; i < arr_size - 2; i++)
-    {
-        l = i + 1;
-
-        r = arr_size - 1;
-        while (l < r)
-        {
-            if (((A[i] + A[l] + A[r]) % 3) == 0)
-            {
-                printf("%d %d %d", A[i],
-                       A[l], A[r]);
-                return true;
-            }
-            else if (((A[i] + A[l] + A[r]) % 3) < sum)
-                l++;
-            else
-                r--;
-        }
-    }
-    return false;
-}
-
 int main()
 {
     int n, i, l, r, *data;
